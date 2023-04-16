43d8404075578d61c512695c8ae37b672ea32f6b
bharathsandepogu01 <bharathsandepogu01@gmail.com>
2023-04-16 10:00:30 +0530

    minor(header-nav): header scroll display and nav mobile responsive and style changes

--- src/components/AppLayout/Header/index.js
+++ src/components/AppLayout/Header/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import Logo from '@components/Logo';
 import HamBurgerMenu from '@components/HamburgerMenu';
 import classes from './styles.module.scss';
 import ThemeButton from '@components/ThemeButton';
 
 function Header() {
+  const headerRef = useRef();
+  const prevScrollY = useRef(0);
+
+  useEffect(() => {
+    const scrollHandler = e => {
+      const currScrollY = window.scrollY;
+      if (currScrollY > prevScrollY.current) {
+        headerRef.current.classList.add(classes.hideHeaderContainer);
+      } else {
+        headerRef.current.classList.remove(classes.hideHeaderContainer);
+      }
+      prevScrollY.current = currScrollY;
+    };
+
+    window.addEventListener('scroll', scrollHandler);
+
+    return () => {
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+
   return (
-    <header className={classes.headerContainer}>
+    <header className={classes.headerContainer} ref={headerRef}>
       <Logo />
       <HamBurgerMenu />
       <ThemeButton />
--- src/components/HamburgerMenu/index.js
+++ src/components/HamburgerMenu/index.js
@@ -1,45 +1,110 @@
 import React, {useEffect, useRef, useState} from 'react';
 
 import classes from './styles.module.scss';
+import useWindowSize from '@hooks/useWindowSize';
+import Modal from '@components/Modal';
 
 function HamBurgerMenu() {
   const [expandMenu, setExpandMenu] = useState(false);
   const backgroundRef = useRef();
   const hamburgerRef = useRef();
   const linkContainerRef = useRef();
+  const mobileMenuLinks = useRef();
+
+  const {isMobileMenuScreen} = useWindowSize();
 
   useEffect(() => {
-    if (!expandMenu) {
+    if (!isMobileMenuScreen) {
+      if (!expandMenu) {
+        hamburgerRef.current.classList.remove(classes.hamBurgerActive);
+        linkContainerRef.current.classList.remove(classes.displayLinks);
+        backgroundRef.current.classList.remove(classes.expandBackground);
+      } else {
+        hamburgerRef.current.classList.add(classes.hamBurgerActive);
+        backgroundRef.current.classList.add(classes.expandBackground);
+        linkContainerRef.current.classList.add(classes.displayLinks);
+      }
+    } else {
       hamburgerRef.current.classList.remove(classes.hamBurgerActive);
       linkContainerRef.current.classList.remove(classes.displayLinks);
       backgroundRef.current.classList.remove(classes.expandBackground);
-    } else {
-      hamburgerRef.current.classList.add(classes.hamBurgerActive);
-      backgroundRef.current.classList.add(classes.expandBackground);
-      linkContainerRef.current.classList.add(classes.displayLinks);
+      if (!expandMenu) {
+        mobileMenuLinks.current.classList.remove(classes.displayMobileLinks);
+      } else {
+        mobileMenuLinks.current.classList.add(classes.displayMobileLinks);
+      }
     }
-  }, [expandMenu]);
+  }, [expandMenu, isMobileMenuScreen]);
 
   const handleOnClickMenu = () => {
     setExpandMenu(!expandMenu);
   };
+
   return (
-    <div className={classes.menuContainer}>
-      <ul className={classes.linksContainer} ref={linkContainerRef}>
-        <li>Home</li>
-        <li>About</li>
-        <li>Experience</li>
-        <li>Projects</li>
-        <li>Blogs</li>
+    <nav className={classes.menuContainer}>
+      <ul
+        className={classes.linksContainer}
+        ref={linkContainerRef}
+        aria-label={'links'}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Experience</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Blogs</a>
+        </li>
       </ul>
       <button
         className={classes.hamburger}
         ref={hamburgerRef}
-        onClick={handleOnClickMenu}>
+        onClick={handleOnClickMenu}
+        aria-label={`${expandMenu ? 'close menu' : 'open menu'}`}>
         <span />
       </button>
-      <span className={classes.expandableBackground} ref={backgroundRef} />
-    </div>
+      <span
+        className={classes.expandableBackground}
+        ref={backgroundRef}
+        aria-hidden
+      />
+      <Modal showModal={expandMenu && isMobileMenuScreen}>
+        <nav className={classes.mobileMenuContainer}>
+          <button
+            className={classes.cancelIcon}
+            aria-label="close menu"
+            onClick={handleOnClickMenu}>
+            <span aria-hidden />
+          </button>
+          <ul
+            className={classes.mobileLinksContainer}
+            aria-label={'links'}
+            ref={mobileMenuLinks}>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Experience</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Blogs</a>
+            </li>
+          </ul>
+        </nav>
+      </Modal>
+    </nav>
   );
 }
 
