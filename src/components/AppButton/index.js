58a84fe63710424617482d2fd90e891aff23df67
bharathsandepogu01 <bharathsandepogu01@gmail.com>
2023-04-26 21:03:47 +0530

    minor(app-button-v1): app button with primary and secondary types

---
+++ src/components/AppButton/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import clsx from 'clsx';
+import AppText from '@components/AppText';
+import classes from './styles.module.scss';
+
+function AppButton(props) {
+  const {primary, secondary, btnText, onClick} = props;
+
+  const classNames = clsx(
+    classes.button,
+    primary && classes.primaryBtn,
+    secondary && classes.secondaryBtn,
+  );
+
+  const btnTextClassNames = clsx(
+    primary && classes.primaryBtnText,
+    secondary && classes.secondaryBtnText,
+  );
+
+  return (
+    <button className={classNames} onClick={() => onClick && onClick()}>
+      <AppText variant="span" small customClassName={btnTextClassNames}>
+        {btnText}
+      </AppText>
+    </button>
+  );
+}
+
+export default AppButton;
