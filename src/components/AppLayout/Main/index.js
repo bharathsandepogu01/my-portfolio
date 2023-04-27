4343a49261cdd792b858053518ba304e0e56de57
bharathsandepogu01 <bharathsandepogu01@gmail.com>
2023-04-27 17:55:07 +0530

    minor(experience-section): added experience section

--- src/components/AppLayout/Main/index.js
+++ src/components/AppLayout/Main/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeaderSection from '@components/HeaderSection';
+import ExperienceSection from '@components/ExperienceSection';
 import classes from './styles.module.scss';
 
 function Main() {
   return (
     <main className={classes.mainContainer}>
       <HeaderSection />
+      <ExperienceSection />
     </main>
   );
 }
--- src/components/AppSection/index.js
+++ src/components/AppSection/index.js
@@ -5,7 +5,28 @@ import AppText from '@components/AppText';
 function AppSection(props) {
   const sectionLineRef = useRef();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (!props.headerTxt) return;
+    const obsCallBack = function (entries, observer) {
+      const [entry] = entries;
+      if (!sectionLineRef.current) return;
+      if (entry.isIntersecting) {
+        sectionLineRef.current.classList.add(classes.expandSectionLine);
+      } else {
+        sectionLineRef.current.classList.remove(classes.expandSectionLine);
+      }
+    };
+    const obsOptions = {
+      root: null,
+      threshold: 0,
+    };
+    const observer = new IntersectionObserver(obsCallBack, obsOptions);
+    observer.observe(sectionLineRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [props.headerTxt]);
 
   return (
     <section className={classes.sectionContainer}>
